7397fad416a15aafc00ef55c6ca64072f9ca3957
Nikesh Kalu <nikesh.kalu@deerwalk.edu.np>
2019-03-01 07:22:34 -0800

    Project Added

---
+++ CRUDEoperation/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCMXUZ6qHcknnQ-r6JLW262WruVb1KEhYY",
+    authDomain: "fir-crudappwithangularjs.firebaseapp.com",
+    databaseURL: "https://fir-crudappwithangularjs.firebaseio.com",
+    projectId: "fir-crudappwithangularjs",
+    storageBucket: "fir-crudappwithangularjs.appspot.com",
+    messagingSenderId: "1023466851682"
+  }
+};
+
+
